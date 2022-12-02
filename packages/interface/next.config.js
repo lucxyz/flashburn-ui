2b89c8f617a7832935f9ffe8e68c036c1c27d667
lucxyz <lucxy@mailfence.com>
2022-12-02 08:07:24 +0100

    Disable image optimisation for static page

--- packages/interface/next.config.js
+++ packages/interface/next.config.js
@@ -1,5 +1,9 @@
 const withTM = require("next-transpile-modules")(["@snx-flash-tool/contracts"]);
 
 module.exports = withTM({
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 });
