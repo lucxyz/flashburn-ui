d5ea02ad6f60df3628de2caa7bfe5eb12a681a9d
lucxyz <119551028+lucxyz@users.noreply.github.com>
2023-08-17 17:07:34 +0200

    Update index.ts

--- packages/interface/src/constants/index.ts
+++ packages/interface/src/constants/index.ts
@@ -1,3 +1,3 @@
 export const siteURL: string = process.env.NEXT_PUBLIC_SITE_URL ?? "";
 export const INFURA_ID: string = process.env.NEXT_PUBLIC_INFURA_ID ?? "";
-export const oneinchKey: string = process.env.ONEINCH_KEY;
+export const oneinchKey: string = process.env.ONEINCH_KEY ?? "";
