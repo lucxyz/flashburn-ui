f829b6f6924cec162bca4dd81047f61eb447a556
lucxyz <119551028+lucxyz@users.noreply.github.com>
2023-11-27 19:58:54 +0100

    Update setupProxy.js

--- packages/interface/src/setupProxy.js
+++ packages/interface/src/setupProxy.js
@@ -7,11 +7,6 @@ module.exports = function (app) {
     target: "https://api.1inch.dev/swap",
     changeOrigin: true,
     logLevel: 'debug',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
-    },
     onProxyReq: (proxyReq) => {
       // add API key in Header
         proxyReq.setHeader(
