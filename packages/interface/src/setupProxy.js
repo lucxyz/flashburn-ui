e3588569b6d68f19a158ed207b5a634e8d5dfd3f
lucxyz <119551028+lucxyz@users.noreply.github.com>
2023-11-27 18:35:04 +0100

    Update setupProxy.js

--- packages/interface/src/setupProxy.js
+++ packages/interface/src/setupProxy.js
@@ -3,18 +3,16 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
 
 module.exports = function (app) {
-
-app.use( "/swap",
-createProxyMiddleware({
-  target: "https://api.1inch.dev/swap",
-  changeOrigin: true,
-  onProxyReq: (proxyReq) => {
-    // add API key in Header
-    proxyReq.setHeader(
-      "Authorization",
-      `Bearer ${process.env.REACT_APP_1INCH_KEY}`
-      );
-    },
-  })
- );
+  app.use(createProxyMiddleware("/swap", {
+    target: "https://api.1inch.dev/swap",
+    changeOrigin: true,
+    onProxyReq: (proxyReq) => {
+      // add API key in Header
+        proxyReq.setHeader(
+          "Authorization",
+          `Bearer ${process.env.REACT_APP_1INCH_KEY}`
+        );
+      },
+    })
+   );
 };
