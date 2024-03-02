f8f7fc6adacbeab2e20f0c42d6c09e1854a3092e
lucxyz <119551028+lucxyz@users.noreply.github.com>
2024-03-02 01:44:57 +0100

    Update setupProxy.js

--- packages/interface/src/setupProxy.js
+++ packages/interface/src/setupProxy.js
@@ -4,17 +4,29 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 // for your API key in .env
 module.exports = function (app) {
 
-app.use( "/v5.2",
-createProxyMiddleware({
-  target: "https://api.1inch.dev",
-  changeOrigin: true,
-  onProxyReq: (proxyReq) => {
-    // add API key in Header
-    proxyReq.setHeader(
-      "Authorization",
-      `Bearer ${process.env.ONEINCH_KEY}`
-      );
-    },
-  })
- );
-};
+
+  app.use( "/quote", createProxyMiddleware({
+    target: "https://api.1inch.dev",
+    changeOrigin: true,
+    onProxyReq: (proxyReq) => {
+      // add API key in Header
+      proxyReq.setHeader(
+        "Authorization",
+        `Bearer ${process.env.ONEINCH_KEY}`
+        );
+      },
+    })
+   );
+    app.use( "/swap", createProxyMiddleware({
+    target: "https://api.1inch.dev",
+    changeOrigin: true,
+    onProxyReq: (proxyReq) => {
+      // add API key in Header
+      proxyReq.setHeader(
+        "Authorization",
+        `Bearer ${process.env.ONEINCH_KEY}`
+        );
+      },
+    })
+   );
+  };
